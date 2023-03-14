1b2323d9a47a306b90d7094366c8ae5f71d14d75
juank <juankamilocromero@gmail.com>
2023-03-14 12:20:44 -0500

    Ejercicio sin terminar.

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre:string,
+  poder : number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  agregar(event: any){
-    event.preventDefault()
-    console.log('Esta es una prueba ')
+
+  personajes:Personaje[]=[
+    {
+      nombre: "Goku",
+      poder:15000
+    },
+    {
+      nombre: "Vegeta",
+      poder: 7500
+    }
+  ]
+  nuevo:Personaje ={
+    nombre: '',
+    poder: 0
+  }
+
+  cambiarNombre(event:any){
+    console.log(event.target.value)
+  }
+
+  agregar(){
+    // event.preventDefault()
+    // console.log(this.nuevo)
+    if(this.nuevo.nombre.trim().length===0){
+      return;
+    }
+    console.log(this.nuevo);
+    
 
   }
 
